0b34d2c4b04671036aebe21849083bf3ac451139
miethaw <32254764+aungkyawnyuntlashio@users.noreply.github.com>
2021-05-08 11:04:52 +0630

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express(); app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-const port = 8080; 
+const port = 8081; 
 app.listen(port, error => {
     console.log("Server is started on port "+ port)
 });
