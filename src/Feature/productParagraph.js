cf2dfc7ffa4ab473bd19631069ae7676a753bf89
Khin-sandar <khinlay10111997@gmail.com>
2021-05-18 17:51:17 +0630

    added new product component

--- src/Feature/productParagraph.js
+++ src/Feature/productParagraph.js
@@ -1,34 +1,69 @@
-import React, { useEffect, useState } from 'react'
-import Font from './fonts'
-import Color from './colors'
+// import React, { useEffect, useState } from 'react'
+// import Font from './fonts'
+// import Color from './colors'
 
-const ProductParagraph = (props) => {
-    const { imgRadius, isReverse, imgSrc, imageWidth, data,imgheight } = props;
-    const [pageWidth, setPageWidth] = useState(window.innerWidth)
+// const ProductParagraph = (props) => {
+//     const { imgRadius, isReverse, imgSrc, imageWidth, data,imgheight } = props;
+//     const [pageWidth, setPageWidth] = useState(window.innerWidth)
 
-    useEffect(() => {
-        window.onresize = (e) => {
-            setPageWidth(e.target.innerWidth)
-        }
-    })
+//     useEffect(() => {
+//         window.onresize = (e) => {
+//             setPageWidth(e.target.innerWidth)
+//         }
+//     })
+
+//     return (
+//         <div className='my-4'>
+//             <div className={`d-flex ${isReverse ? 'flex-row-reverse' : 'flex-row'} flex-xl-nowrap flex-lg-nowrap flex-md-nowrap flex-sm-wrap flex-xs-wrap flex-wrap mx-4 justify-content-center`} >
+//                 <div className='row ms-3 me-2'>
+//                 <div className='col-6 image-box' style={{ display: 'block'}}>
+//                     <img className='shadow' src={imgSrc} alt="img" style={{ width: imageWidth, borderRadius: imgRadius,height:imgheight, border: '1px solid #C5B3E5' }} />
+//                 </div>
+//                 <div className='col-5 ms-3 me-2 m-container' style={{ display: 'block' }}>
+//                     <div /*className={`${pageWidth < 768 ? 'text-right' : isReverse && ' text-end'} ms-5`}*/className='pl-2' style={{ whiteSpace: 'nowrap', color: Color.TitleColor, fontSize: Font.heading12, fontWeight: 400 }}>{data.title}</div>
+//                     <div className={`dropdown-divider m-title border-0 `} style={{ backgroundColor: '', height: "2px"}}></div>
+//                     <div className='ms-3 me-2' style={{color: "#084C95", fontWeight:600,fontSize: '15px' }}>
+//                 <p style={{ }}>{data.description}</p>
+//                 </div>
+//             </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+// export default ProductParagraph;
+
+
+import React from "react";
+import colors from "./colors";
+import fonts from "./fonts";
+
+const ProductParagraph = props => {
+    const { data, imgSrc, imgWidth, isReverse,imgheight } = props
 
     return (
-        <div className='my-4'>
-            <div className={`d-flex ${isReverse ? 'flex-row-reverse' : 'flex-row'} flex-xl-nowrap flex-lg-nowrap flex-md-nowrap flex-sm-wrap flex-xs-wrap flex-wrap mx-4 justify-content-center`} >
-                <div className='row ms-3 me-2'>
-                <div className='col-6 image-box' style={{ display: 'block'}}>
-                    <img className='shadow' src={imgSrc} alt="img" style={{ width: imageWidth, borderRadius: imgRadius,height:imgheight, border: '1px solid #C5B3E5' }} />
-                </div>
-                <div className='col-5 ms-3 me-2 m-container' style={{ display: 'block' }}>
-                    <div /*className={`${pageWidth < 768 ? 'text-right' : isReverse && ' text-end'} ms-5`}*/className='pl-2' style={{ whiteSpace: 'nowrap', color: Color.TitleColor, fontSize: Font.heading12, fontWeight: 400 }}>{data.title}</div>
-                    <div className={`dropdown-divider m-title border-0 `} style={{ backgroundColor: '', height: "2px"}}></div>
-                    <div className='ms-3 me-2' style={{color: "#084C95", fontWeight:600,fontSize: '15px' }}>
-                <p style={{ }}>{data.description}</p>
+        < div  style={{ margin: '30px 30px 0 0' }}>
+            <div className='image-box' style={{ float: isReverse ? 'left' : 'right' }}><img className='kumo-image' src={imgSrc} style={{
+                display: 'block', width: imgWidth,
+                height:imgheight,
+                // height: '185px',
+                boxShadow: '10px 10px 10px #00000029',
+                borderRadius: '41px',
+                opacity: 1,
+                // marginTop: -30
+
+            }} /></div>
+            <div className="m-container">
+                <h3 className="title" style={{ color: '#084C95', textAlign: isReverse ? 'right' : 'left', font: 'normal normal normal 30px/36px Microsoft Sans Serif' }} >{data.title}</h3>
+                <div className="dropdown-divider-1 ud_line p-0 m-0 m-title mb-2" style={{ display: 'block' }}></div>
+                <div className='m-paragraph'>
+                    <p style={{ color: colors.ParagraphColor }}>{data.description1}</p>
+                    <p style={{ color: colors.ParagraphColor }}>{data.description2}</p>
+                    <p style={{ color: colors.ParagraphColor }}>{data.description3}</p>
                 </div>
             </div>
-                </div>
-            </div>
-        </div>
+        </div >
     )
 }
-export default ProductParagraph;
+
+export default ProductParagraph
