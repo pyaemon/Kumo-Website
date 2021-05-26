a07c9c8a4e43c5215f359b87945a50f5525f12b0
Khin-sandar <khinlay10111997@gmail.com>
2021-05-26 08:20:16 +0630

    our service

--- src/Feature/OurSevice.js
+++ src/Feature/OurSevice.js
@@ -12,7 +12,7 @@ const OurService = () => {
 
                     <div className="col-6 col-sm-6 col-md-4 col-lg-2 col-xl-2 col-xxl-1 p-0 m-0">
                         <div className="p-2">
-                            <div className="shadow m-0" onClick={handleClickDtrAnz} style={{ height: 110, border: "1px solid #4B74AD", borderRadius: 30, paddingTop: 15, background: "#FFFFFF" }}>
+                            <div className="shadow m-0" onClick={handleClickDtrAnz} style={{ height: 120, border: "1px solid #4B74AD", borderRadius: 30, paddingTop: 15, background: "#FFFFFF" }}>
                                 <img className="center mt-3" src={"/icons/line_chart.png"} alt="view" style={{ background: "#084c95", borderRadius: 10, display: "block", margin: "auto", height: "50px" }} />
                                 <div className="text-center" style={{ paddingTop: 15, }}>
                                     <span className="service-text" >Data Analytics</span>
@@ -22,7 +22,7 @@ const OurService = () => {
                     </div>
                     <div className="col-6 col-sm-6 col-md-4 col-lg-2 col-xl-2 col-xxl-1 p-0 m-0">
                         <div className="p-2">
-                            <div className="shadow m-0" onClick={handleClickDtrAnz} style={{ height: 110, border: "1px solid #4B74AD", borderRadius: 30, paddingTop: 15, background: "#FFFFFF" }}>
+                            <div className="shadow m-0" onClick={handleClickDtrAnz} style={{ height: 120, border: "1px solid #4B74AD", borderRadius: 30, paddingTop: 15, background: "#FFFFFF" }}>
                                 <img src={"/icons/globe.png"} alt="view" style={{ display: "block", margin: "auto" }} />
                                 <div className="text-center" style={{ paddingTop: 15 }}>
                                     <span className="service-text" >Web Development</span>
@@ -32,7 +32,7 @@ const OurService = () => {
                     </div>
                     <div className="col-6 col-sm-6 col-md-4 col-lg-2 col-xl-2 col-xxl-1 p-0 m-0">
                         <div className="p-2">
-                            <div className="shadow m-0" onClick={handleClickDtrAnz} style={{ height: 110, border: "1px solid #4B74AD", borderRadius: 30, paddingTop: 15, background: "#FFFFFF" }}>
+                            <div className="shadow m-0" onClick={handleClickDtrAnz} style={{ height: 120, border: "1px solid #4B74AD", borderRadius: 30, paddingTop: 15, background: "#FFFFFF" }}>
                                 <img src={"/icons/iphone.png"} alt="view" style={{ display: "block", margin: "auto" }} />
                                 <div className="text-center" style={{ paddingTop: 15 }}>
                                     <span className="service-text" >Mobile Development</span>
@@ -42,7 +42,7 @@ const OurService = () => {
                     </div>
                     <div className="col-6 col-sm-6 col-md-4 col-lg-2 col-xl-2 col-xxl-1 p-0 m-0">
                         <div className="p-2">
-                            <div className="shadow m-0" onClick={handleClickDtrAnz} style={{ height: 110, border: "1px solid #4B74AD", borderRadius: 30, paddingTop: 15, background: "#FFFFFF" }}>
+                            <div className="shadow m-0" onClick={handleClickDtrAnz} style={{ height: 120, border: "1px solid #4B74AD", borderRadius: 30, paddingTop: 15, background: "#FFFFFF" }}>
                                 <img src={"/icons/laptop.png"} alt="view" style={{ display: "block", margin: "auto" }} />
                                 <div className="text-center" style={{ paddingTop: 15 }}>
                                     <span className="service-text" >UI/UX Design</span>
@@ -52,7 +52,7 @@ const OurService = () => {
                     </div>
                     <div className="col-6 col-sm-6 col-md-4 col-lg-2 col-xl-2 col-xxl-1 p-0 m-0">
                         <div className="p-2">
-                            <div className="shadow m-0" onClick={handleClickDtrAnz} style={{ height: 110, border: "1px solid #4B74AD", borderRadius: 30, paddingTop: 15, background: "#FFFFFF" }}>
+                            <div className="shadow m-0" onClick={handleClickDtrAnz} style={{ height: 120, border: "1px solid #4B74AD", borderRadius: 30, paddingTop: 15, background: "#FFFFFF" }}>
                                 <img src={"/icons/hangar.png"} alt="view" style={{ display: "block", margin: "auto" }} />
                                 <div className="text-center" style={{ paddingTop: 15 }}>
                                     <span className="service-text" >Data Warehousing</span>
